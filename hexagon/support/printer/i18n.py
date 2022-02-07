fd793346e06c70b586b114cb512b04a102a48750
Joaco Campero <joaquin@redb.ee>
2022-02-07 20:10:59 -0300

    fix(i18n): search local and system dirs
    lookup is done:
     1. HEXAGON_LOCALES_DIR if present
     2. local install dir (~/.local)
     3. local install dir (~/.local) with default language
     4. system install dir (/usr/local)
     5. system install dir (/usr/local) with default language
     6. default gettext lookup dir or fallback

---
+++ hexagon/support/printer/i18n.py
@@ -0,0 +1,37 @@
+import os
+import site
+import sys
+
+import gettext
+
+LOCALEDIR = os.environ.get("HEXAGON_LOCALES_DIR", None)
+
+# site.USER_BASE is usually ~/.local (this is used when pip WAS NOT installed globally)
+LOCAL_LOCALEDIR = os.path.join(site.USER_BASE, "locales")
+
+# sys.prefix is usually /usr/local (this is used when pip WAS installed globally)
+SYSTEM_LOCALEDIR = os.path.join(sys.prefix, "locales")
+
+DEFAULT_LANGUAGE = "en"
+
+
+def install():
+    el = (
+        (__try_translation(localedir=LOCALEDIR) if LOCALEDIR else None)
+        or __try_translation(localedir=LOCAL_LOCALEDIR)
+        or __try_translation(localedir=LOCAL_LOCALEDIR, languages=[DEFAULT_LANGUAGE])
+        or __try_translation(localedir=SYSTEM_LOCALEDIR)
+        or __try_translation(localedir=SYSTEM_LOCALEDIR, languages=[DEFAULT_LANGUAGE])
+        or __try_translation(fallback=True)
+    )
+
+    el.install()
+
+    return el.gettext
+
+
+def __try_translation(**kwargs):
+    try:
+        return gettext.translation("hexagon", **kwargs)
+    except FileNotFoundError:
+        return None
